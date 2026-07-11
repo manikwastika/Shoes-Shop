ae073bb3b1ed9620bb7fc8a261fda62763c5e3c9
Manik <manikwastikafors@gmail.com>
2026-07-11 11:46:43 +0800

    alah sia boy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
+  base: "/Shoes-Shop/",
   plugins: [react(), tailwindcss(), flowbiteReact()],
 });
